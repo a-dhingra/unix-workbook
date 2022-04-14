6ee49bec9ab05efae86f08213320fe662633e1ec
Anupam Dhingra <33241303+a-dhingra@users.noreply.github.com>
2022-04-13 22:13:02 -0400

    Update guessinggame.sh
    Updated the bash script to cover edge cases

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#!/bin/env
 
 file_count=$(ls | wc -l)
-# echo "The file count is $file_count"
 
+echo "Let's play a game"
 echo "You need to guess the number of files in this directory"
+
+function logic {
+        if [[ $1 -ne $2 ]]; then
+                if [[ $1 -gt $2 ]]; then
+                        echo
+                        echo "Your guess is HIGHER. Try Again..."
+                else
+                        echo
+                        echo "Your guess is LOWER. Try Again..."
+                fi
+        else
+                echo "************************************"
+                echo "CONGRATUATIONS! You guessed it right!"
+                echo "************************************"
+        fi
+}
+
 
 while [[ $response -ne $file_count ]]
 do
         echo
         echo "What is your GUESS?"
         read response
-        if [[ $response -gt $file_count ]]; then
-                echo
-                echo "Your guess is HIGHER. Try Again..."
+        if [[ $response == +([0-9]) ]]; then
+                logic $response $file_count
         else
-                echo
-                echo "Your guess is LOWER. Try Again..."
+                echo "Invalid input! Please try again"
         fi
 done
-
-echo
-echo "*************************************"
-echo "CONGRATULATIONS! You guessed it right!"
-echo "*************************************"
