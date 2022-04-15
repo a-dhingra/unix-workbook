2d68585c845cd2e993b0ac53c7ea65458ab7f908
Anupam Dhingra <33241303+a-dhingra@users.noreply.github.com>
2022-04-14 23:57:34 -0400

    Update guessinggame.sh
    - fixed Shebang
    - added checks for invalid inputs
    - included hidden files and directories in calculations

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,18 @@
-#!/bin/env
+#!/usr/bin/env
 
-file_count=$(ls | wc -l)
+all_files=( * )            # includes directories
+directories=( */ )         # directories only
+hidden_files=( .* )        # includes directories
+hidden_directories=( .*/ ) # directories only
 
+Number_of_files=$(( ${#all_files[@]} - ${#all_directories[@]} ))
+# Number_of_directories=${#directories[@]}
+Number_of_hidden_files=$(( ${#hidden_files[@]} - ${#hidden_directories[@]} ))
+# Number_of_hidden_directories=$(( ${#hidden_directories[@]} - 2 ))
+
+file_count=$((Number_of_files + Number_of_hidden_files))
+
+echo
 echo "Let's play a game"
 echo "You need to guess the number of files in this directory"
 
@@ -9,10 +20,10 @@ function logic {
         if [[ $1 -ne $2 ]]; then
                 if [[ $1 -gt $2 ]]; then
                         echo
-                        echo "Your guess is HIGHER. Try Again..."
+                        echo "Your guess is HIGHER than the correct answer. Try Again..."
                 else
                         echo
-                        echo "Your guess is LOWER. Try Again..."
+                        echo "Your guess is LOWER than the correct answer. Try Again..."
                 fi
         else
                 echo "************************************"
@@ -21,15 +32,17 @@ function logic {
         fi
 }
 
-
 while [[ $response -ne $file_count ]]
 do
         echo
         echo "What is your GUESS?"
         read response
-        if [[ $response == +([0-9]) ]]; then
-                logic $response $file_count
+		if  [[ $response == *[a-zA-Z'!'\@#\$\%\^\&\*\(\)_+]* ]]; then
+				echo "Invalid input! Please try again"
+				response=0
+        #if [[ $response == +([0-9]) ]]; then
+        #        logic $response $file_count
         else
-                echo "Invalid input! Please try again"
+				logic $response $file_count
         fi
 done
